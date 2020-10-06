c613959d9d3fa6d75667cf6212ed2d67131d9903
lenovo 3 <lenovo 3>
2020-10-06 10:12:06 +0200

    US1: each deposit increase the account balance

--- src/account.spec.js
+++ src/account.spec.js
@@ -4,9 +4,14 @@ describe('Test our  test tool JEST', () => {
     });
 });
 
+var account = {balance : 0};
+
+const deposit = (moneyAmount) => account.balance += moneyAmount;
+
 
 describe('Deposit money from a customer to his account', () => {
-    test('', () => {
-        expect(acount.deposit())
-    });
+    test('if a customer deposit money to his account otherwise ' +
+        'his account balance will increase by the amount of the deposit',() => {
+            expect(deposit(1)).toStrictEqual( 1)
+        });
 });
